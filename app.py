c1f9613568a2ec158502ec59b294cd210499095f
Amin Zamani <abz@email.unc.edu>
2021-10-23 22:06:00 -0400

    login/signup complete

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, send_file, session
+from flask import Flask, request, send_file, session, redirect, url_for, render_template
 import os
 import json
 import firebase_admin
@@ -11,14 +11,21 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 base = os.getcwd()
 app.config['UPLOAD_FOLDER'] = base +'/static'
 
-f_credential = json.loads(os.environ['GOOGLE_APPLICATION_CREDS'])
+#f_credential = json.loads(os.environ['GOOGLE_APPLICATION_CREDS'])
+f = open('secret.json')
+f_credential = json.load(f)
+f.close()
 f_credentials = firebase_admin.credentials.Certificate(f_credential)
 fb = firebase_admin.initialize_app(f_credentials, {'databaseURL': 'https://riggy-72c18-default-rtdb.firebaseio.com/'})
 
 from firebase_admin import db as database
 
+def keygen(user, passw):
+    user = user.replace('@', '!!').replace('.','!')
+    key = user + "|" + passw
+    return key
 def login(user, passw):
-    key = user + ":" + passw
+    key = keygen(user, passw)
     if key in database.reference('/users').get(shallow=True):
         session["is_logged_in"] = True
         session["user"] = user
@@ -27,17 +34,23 @@ def login(user, passw):
         return True
     return False
 
-def createUser(user, passw, type):
-    key = user + ":" + passw
+def createUser(user, passw, type, name, address1, address2, city, state, zip):
+    key = keygen(user, passw)
     ref = database.reference('/users/' + key)
-    ref.update({"dateCreated": datetime.now()})
-    ref.update({"type": type})
+    ref.update({"dateCreated": datetime.now().strftime("%m/%d/%Y;%H:%M:%S")})
+    ref.update({"type": type,
+                "name": name,
+                "address1": address1,
+                "address2": address2,
+                "city": city,
+                "state": state,
+                "zip": zip})
 
-def signUp(user, passw, type):
-    createUser(user, passw, type)
+def signUp(user, passw, type, name, address1, address2, city, state, zip):
+    createUser(user, passw, type, name, address1, address2, city, state, zip)
     login(user, passw)
 
-def signOut(user, passw):
+def signOut():
     if "is_logged_in" in session and session["is_logged_in"]:
         session["is_logged_in"] = False
         del session["user"]
@@ -46,10 +59,47 @@ def signOut(user, passw):
         return True
     return False
 
+def isLoggedIn():
+    return "is_logged_in" in session and session['is_logged_in']
+
 @app.route('/')
-def hello_world():
+def home():
+    if not isLoggedIn():
+        return redirect('/login')
     return 'Hello, World'
 
+@app.route('/signOut')
+def signOutP():
+    res = signOut()
+    return redirect('/')
+@app.route("/login", methods = ["POST", "GET"])
+def loginP():
+    if request.method == "POST":
+        result = request.form
+        email = result["email"]
+        password = result["pass"]
+        login(email, password)
+        return redirect('/')
+    else:
+        return render_template('login.html')
+
+
+@app.route("/register", methods = ["POST", "GET"])
+def register():
+    if request.method == "POST":
+        result = request.form
+        email = result["email"]
+        password = result["pass"]
+        name = result["name"]
+        address_1 = result["address1"]
+        address_2 = result["address2"]
+        city = result["city"]
+        state = result["state"]
+        zip = result["zip"]
+        account_type = result["account_type"]
+        signUp(email, password, account_type, name, address_1, address_2, city, state, zip)
+        return redirect('/')
+    else:
+        return render_template('signup.html')
 if __name__ == '__main__':
-    session.clear()
-    app.run()
+    app.run(debug=True)
